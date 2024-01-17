c08a5c3c44bcb0cccbf84675eb1b829779747c8b
Sharafudeen jafani Sharafudeen <esjafani@gmail.com>
2024-01-17 18:14:02 +0530

    last

--- script.js
+++ script.js
@@ -37,3 +37,68 @@ function submitForm() {
 }
 
 // QUESTION 4
+
+
+// question5
+var image=["http://farm4.staticflickr.com/3691/11268502654_f28f05966c_m.jpg","http://farm1.staticflickr.com/33/45336904_1aef569b30_n.jpg","http://farm6.staticflickr.com/5211/5384592886_80a512e2c9.jpg" ]
+var index=0;
+function changeimage(){
+    index++;
+if(image==index.length){
+    index=0;
+}
+    const img=document.getElementById("image");
+    img.src=image[index]
+}
+// question6
+function openurl(){
+    window.open("https://webapp.entri.app/home")
+}
+
+// question7
+function redirect() {
+    window.location.href=("https://www.youtube.com/");
+}
+// question8
+
+function displyurl(){
+    
+    const protocol=window.location.protocol; 
+  const host =window.location.hostname ;
+  const searchParams= new URLSearchParams(window.location.search);
+  const result=(protocol+","+ host+","+searchParams)
+  document.getElementById("info").innerText = result;
+
+  console.log(result);
+}
+
+// question 9
+function reload(){
+    window.location.reload()
+}
+// question 10
+function handw(){
+   let widthh= window.screen.width;
+   let heightt= window.screen.height;
+   const  relt=document.getElementById("question10");
+   relt.innerText="width="+ widthh+",height="+heightt;
+
+console.log(relt);
+}
+
+// Question 11
+    let count = 10;
+
+    function start() {
+      function updateCountdown() {
+        document.getElementById('count').innerHTML = count + " seconds Left";
+        if (count === 0) {
+          alert("Countdown zero");
+        } else {
+          count--;
+          setTimeout(updateCountdown, 1000);
+        }
+      }
+
+      updateCountdown();
+    }
